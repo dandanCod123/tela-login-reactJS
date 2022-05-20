aae39b5d80d3f1ed034f4c45ee31bc4091a5b432
dandanCod123 <danielmouradesousa5@gmail.com>
2022-05-20 14:14:22 -0300

    tela final

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/Button";
+import useAuth from "../../hooks/userAuth";
+import * as C from "./styles";
 
 const Home = () => {
+    const { signout } = useAuth();
+    const navigate = useNavigate();
+
+
     return(
-        <div>Home</div>
+        <C.Contairne>
+            <C.title>HOME</C.title>
+            <Button text="sair" onClick={() => [signout(), navigate("/")]}>
+                sair
+            </Button>
+        </C.Contairne>
         
     );
 };
